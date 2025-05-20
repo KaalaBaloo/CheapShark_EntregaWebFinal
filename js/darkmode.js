62ae29837163297856b51ab4805903127cfaaf0a
Gabi <gabirademakers@gmail.com>
2025-05-21 01:00:28 +0200

    LandingDarkMode

---
+++ js/darkmode.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleBtn = document.getElementById("modo-toggle");
+    const logoImg = document.querySelector(".logo-img");
+    const fondo = document.body;
+    const botonCTA = document.querySelector("#c2a img");
+
+    let darkMode = false;
+
+    toggleBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      darkMode = !darkMode;
+      fondo.classList.toggle("dark-mode");
+
+      logoImg.src = darkMode
+        ? "../media/img/CheapSharkTituloO.png"
+        : "../media/img/CheapSharkTituloC.png";
+
+      toggleBtn.src = darkMode
+        ? "media/img/ToggleOscuro.png"
+        : "media/img/ToggleClaro.png";
+
+      botonCTA.src = darkMode
+        ? "../media/img/BotonOscuro.png"
+        : "../media/img/BotonClaro.png";
+    });
+  });
